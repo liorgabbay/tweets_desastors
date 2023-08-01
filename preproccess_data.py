2a5c77377e8e65a300094a793fc124c5e247da68
liorgabbay <liorg060@gmail.com>
2023-08-01 12:12:09 +0300

    finish documentation of preprocess test and train

--- preproccess_data.py
+++ preproccess_data.py
@@ -37,6 +37,7 @@
     
     TODO: 
     1. check if it good to delete the location columns
+    2.complete test preprocess
     
 """
 ######## imports ########
@@ -54,15 +55,20 @@ from keras.layers import Embedding, LSTM
 
 ########## globals ##########
 tokenizer = Tokenizer()
+stop_words = set(stopwords.words('english'))
+model = Sequential()
+
+
 #############################
 
 def split_data(data: str):
     """
-    the function shuffle the data and split it to:
-    90% train data
-    10% validation data.
-    :param data: the data which we work on
+    Shuffles the data and splits it into 90% train data and 10% validation data.
+
+    :param data: str
+        Path or file name of the CSV file containing the data.
     """
+
     data = convert_to_csv(data)
     data = data.sample(frac=1)  # shuffle the data
     split_point = int(0.9 * len(data))
@@ -74,19 +80,31 @@ def split_data(data: str):
 
 def convert_to_csv(csv_data: str):
     """
-    convart csv file to DataFrame
-    :param csv_data:
-    :return: DataFrame
-    """
+     Converts a CSV file to a DataFrame.
+
+     Parameters:
+     :param csv_data: str
+         Path or file name of the CSV file to be read.
+
+     Returns:
+     :return: pandas.DataFrame
+         DataFrame containing the data from the CSV file.
+     """
     data = pd.read_csv(csv_data)
     return data
 
 
 def drop_dup_and_columns(data: pd.DataFrame):
     """
-    the function drop the non relevent columns and drop duplicate rows
-    :param data: current working data
-    :return: current data after changes
+    Drops non-relevant columns and duplicate rows from the given DataFrame.
+
+    Parameters:
+    :param data: pd.DataFrame
+        DataFrame containing the data to be processed.
+
+    Returns:
+    :return: pd.DataFrame
+        DataFrame with duplicate rows removed and non-relevant columns ('id' and 'location') dropped.
     """
     data = data.drop_duplicates()
     data = data.drop(columns=['id', 'location'])  # drop id and location columns
@@ -96,25 +114,41 @@ def drop_dup_and_columns(data: pd.DataFrame):
 def keyword_preprocess(data: pd.DataFrame):
     """
     preprocess on the keyword,
-    applying get dummies for this column, because there for similar sample text there is the same
-    keyword, and probably will get the same label.
-    :param data:
-    :return:
+    applying get dummies for this column,
+    (because for similar sample text there is the same keyword,and probably will get the same label)
+
+    :param data: pandas.DataFrame
+        DataFrame containing the 'keyword' column to be preprocessed.
+
+    :return: pandas.DataFrame
+        DataFrame with the 'keyword' column preprocessed using get dummies.
     """
     data = pd.get_dummies(data, columns=["keyword"])
     return data
 
 
-def text_preprocess(data: pd.DataFrame, stop_words_set: set):
+def text_preprocess(data: pd.DataFrame):
     """
-    Preprocesses the text data in the 'text' column of the given DataFrame.
+        Preprocesses the text data in the 'text' column of the given DataFrame.
 
-    :param data: DataFrame containing the text data to be preprocessed.
-    :param stop_words_set: A set of stopwords to be removed from the text data.
+        :param data: pandas.DataFrame
+            DataFrame containing the text data to be preprocessed. It should have a 'text' column
+            containing the preprocessed and tokenized text data.
 
-    :return: DataFrame with word embeddings as additional columns along with the other original columns.
+        :return: pandas.DataFrame
+            DataFrame with word embeddings as additional columns along with the other original columns
+            from the input DataFrame.
+
+        This function takes a DataFrame 'data' containing a column of preprocessed text data and performs
+        the following tasks:
+        1. Cleans the text data in the 'text' column using an 'clean_text' function
+        2. Converts the tokenized and preprocessed text data to numerical form using the word-to-index mapping.
+        3. Applies padding or truncating to sequences to ensure a fixed sequence length.
+        4. Generates word embeddings using a pre-trained model based on the numerical sequences.
+
     """
-    data = clean_text(data, stop_words_set)
+
+    data = clean_text(data)
     # Step 1: Create Word-to-Index Mapping
     tokenizer.fit_on_texts(data['text'])
     word_to_index = tokenizer.word_index
@@ -130,7 +164,6 @@ def text_preprocess(data: pd.DataFrame, stop_words_set: set):
     embedding_dim = 100  # Adjust this according to the desired embedding size
 
     # Build the Model with Training from Scratch Word Embeddings
-    model = Sequential()
     model.add(Embedding(len(word_to_index) + 1, embedding_dim, input_length=max_sequence_length))
     model.add(LSTM(64))
 
@@ -143,31 +176,79 @@ def text_preprocess(data: pd.DataFrame, stop_words_set: set):
     return data
 
 
-def clean_text(data: pd.DataFrame, stop_words_set: set):
+def text_test_preprocess(test_data):
     """
-    doing preprocess on the text column
-    inner functions:
-    1. lower_case
-    2. clean_symbols
-    3. split_text
-    :param stop_words_set: A set of stopwords to be removed from the text data.
-    :param data: current data
-    :return: data after preprocess on the text columns
+      Preprocesses the text data in the 'text' column of the given DataFrame.
+
+      :param test_data: pd.DataFrame
+          DataFrame containing the text data to be preprocessed. It should have a 'text' column
+          containing the preprocessed and tokenized text data.
+
+      :return: pd.DataFrame
+          DataFrame with word embeddings as additional columns along with the other original columns
+          from the input DataFrame.
+
+      This function takes a DataFrame 'test_data' containing a column of preprocessed text data and performs
+      the following tasks:
+      1. Cleans the text data in the 'text' column using an 'clean_text' function
+      2. Converts the tokenized and preprocessed text data to numerical form using the word-to-index mapping
+         of the train data.
+      3. Applies padding or truncating to sequences to ensure a fixed sequence length.
+      4. Generates word embeddings using a pre-trained model based on the numerical sequences.
+
+    """
+    test_data = clean_text(test_data)
+    # convert the text to numrical by the mapping of the train set
+    convertNumerical = tokenizer.texts_to_sequences(test_data['test'])
+    max_sequence_length = 50
+    padded_sequences = pad_sequences(convertNumerical, maxlen=max_sequence_length, padding='post', truncating='post')
+    # predict by the train set model
+    word_embeddings = model.predict(padded_sequences)
+    column_titles = [f'embedding_{i}' for i in range(64)]
+    word_embeddings_df = pd.DataFrame(word_embeddings, columns=column_titles)
+    test_data = pd.concat([word_embeddings_df, test_data.drop(columns=['text'])], axis=1)
+    return test_data
+
+
+def clean_text(data: pd.DataFrame):
+    """
+       Perform text preprocessing on the 'text' column of the given DataFrame.
+
+       Inner Functions:
+       1. lower_text(tweet: str) -> str: Convert the tweet to lowercase.
+       2. clean_symbols(tweet: str) -> str: Remove symbols, URLs, mentions, hashtags, RT tags, HTML entities, and emojis
+          from the tweet.
+       3. split_text(tweet: str) -> List[str]: Split the tweet into individual words.
+       4. clean_stop_words(split_tweet: List[str]) -> List[str]: Remove stopwords from the list of words.
+
+       Parameters:
+       :param data: pandas.DataFrame
+           DataFrame containing the text data to be preprocessed. It should have a 'text' column
+           containing the preprocessed text data as strings.
+
+       Returns:
+       :return: pandas.DataFrame
+           DataFrame with preprocessed text in the 'text' column after removing symbols, URLs, mentions, hashtags,
+            RT tags, HTML entities, emojis, and stopwords.
     """
 
     def lower_text(tweet):
         """
         lower all the letters in the tweet
-        :param tweet: current tweet we work on
-        :return: tweet with lowercase letters
+        :param tweet: str
+            Current tweet to work on.
+        :return: str
+            Tweet with lowercase letters.
         """
         return tweet.lower()
 
     def clean_symbols(tweet):
         """
         remove all the symbols from the tweet
-        :param tweet: current tweet to work on
-        :return: tweet after removing all the symbols.
+        :param tweet: str
+            Current tweet to work on.
+        :return: str
+            Tweet after removing symbols and other entities.
         """
         # Remove URLs
         tweet = re.sub(r'http\S+', '', tweet)
@@ -175,7 +256,7 @@ def clean_text(data: pd.DataFrame, stop_words_set: set):
         # Remove Twitter mentions (@username)
         tweet = re.sub(r'@\w+', '', tweet)
 
-        # Remove hashtags (#word)
+        # Remove hashtags maintain the word (#word)
         tweet = re.sub(r'#(\w+)', r'\1', tweet)
 
         # Remove RT (Retweet) tag
@@ -184,7 +265,7 @@ def clean_text(data: pd.DataFrame, stop_words_set: set):
         # Remove HTML entities like &gt;
         tweet = re.sub(r'&\w+;', '', tweet)
 
-        # Remove emojis (You may need to adapt this regex to your specific case)
+        # Remove emojis
         tweet = re.sub(r'[^\x00-\x7F]+', '', tweet)
 
         # Remove non-letter characters, excluding whitespace
@@ -195,19 +276,35 @@ def clean_text(data: pd.DataFrame, stop_words_set: set):
         return tweet
 
     def split_text(tweet):
+        """
+        Split the tweet into individual words.
+        :param tweet: str
+            Current tweet to work on.
+        :return: List[str]
+            List of words extracted from the tweet.
+        """
+        # split the text pre space
         return tweet.split()
 
     def clean_stop_words(split_tweet):
+        """
+        Remove stopwords from the list of words.
+        :param split_tweet: List[str]
+            List of words in the tweet.
+        :return: List[str]
+            List of words after removing stopwords.
+        """
+        # clean the stop words and conjunctions.
         split_tweet = [word for word in split_tweet if word not in stop_words_set]
         return split_tweet
 
-    # make the text lower case.
+    # make the text lower case
     data['text'] = data['text'].apply(lower_text)
 
     # clean symbols
     data['text'] = data['text'].apply(clean_symbols)
 
-    # split the text.
+    # split the text
     data['text'] = data['text'].apply(split_text)
 
     # clean stop words
@@ -216,48 +313,73 @@ def clean_text(data: pd.DataFrame, stop_words_set: set):
     # combine words back to sentence
     data['text'] = data['text'].apply(lambda words: ' '.join(words))
 
-    #
-    # tfidf_representation = vectorizer.fit_transform(data['text'])
-    #
-    # # Convert TF-IDF representation to DataFrame
-    # tfidf_df = pd.DataFrame(tfidf_representation.toarray(), columns=vectorizer.get_feature_names_out())
-    #
-    # # Combine TF-IDF DataFrame with the other 256 columns
-    # data = pd.concat([tfidf_df, data.drop(columns=['text'])], axis=1)
-
-    # vectorize
-    ## sec way - will create 2D dimension array per entry
-    # vectorizer.fit_transform(data['text'])
-    # data['text'] = data['text'].apply(lambda sentence: vectorizer.transform([sentence]).toarray())
-
-    ### one way
-    # bow_representation = vectorizer.fit_transform(data['text'])
-    #
-    # bow_df = pd.DataFrame(bow_representation.toarray(), columns=vectorizer.get_feature_names_out())
-    #
-    # # Combine BoW DataFrame with the other 256 columns
-    # data = pd.concat([bow_df, data.drop(columns=['text'])], axis=1)
-    ######
-
     return data
 
 
-def train_preprocess(train_data: pd.DataFrame,y_train,stop_words: set):
+def train_preprocess(train_data: pd.DataFrame, y_train: pd.Series):
     """
-    :param stop_words:
-    :param X_data: train data (without the label column)
-    :param y_data: label column
-    :return:
-        1. train Data Frame after preprocess.
-        2. label column after preprocess.
+     Preprocesses the given training data and target labels.
+
+     :param train_data: pandas.DataFrame
+         DataFrame containing the training data, where each row represents a sample and each column represents a feature.
+
+     :param y_train: pandas.DataFrame
+         DataFrame containing the target labels corresponding to the training data.
+
+     :return: Tuple[pd.DataFrame, pd.Series, pd.Series]
+         A tuple containing three elements:
+         1. train_X: pd.DataFrame
+             DataFrame containing the preprocessed features (excluding the target labels).
+         2. train_y: pd.Series
+             DataFrame containing the target labels.
+         3. avg: pd.Series
+             Series containing the average values of each feature in 'train_X' after preprocessing.
     """
-    data = pd.concat([train_data, y_train], axis=1)  # concat the result column to the data
+
+    # concat the result column to the data
+    data = pd.concat([train_data, y_train], axis=1)
+
+    # drop the columns and drop duplicate samples
     data = drop_dup_and_columns(data)
+
+    # preprocess on the keyword column
     data = keyword_preprocess(data)
-    data = text_preprocess(data, stop_words)
+
+    # preprocess on the text column
+    data = text_preprocess(data)
     train_y = data.loc[:, y_train.columns]
     train_X = data.drop(columns=y_train.columns)
-    return train_X,train_y
+    avg = train_X.mean(axis=0)
+    return train_X, train_y,avg
+
+
+def preprocess_test(test_data: pd.DataFrame, avg_train):
+    """
+       Preprocesses the given test data to prepare it for prediction using a trained model.
+
+       :param test_data: pd.DataFrame
+           DataFrame containing the test data, where each row represents a sample and each column represents a feature.
+
+       :param avg_train: pd.Series
+           Series containing the average values of each feature in the training data after preprocessing.
+            The index of the Series should match the column names of the preprocessed training data.
+
+       :return: pd.DataFrame
+           DataFrame containing the preprocessed test data, aligned with the features used for training.
+    """
+
+    # drop id and location columns
+    test_data = test_data.drop(columns=['id', 'location'])
+
+    # preprocess on the keyword column
+    test_data = keyword_preprocess(test_data)
+
+    # preprocess on the text column
+    test_data = text_test_preprocess(test_data)
+
+    # match the column of the test to the column of the train
+    test_data = test_data.reindex(columns=avg_train.index, fill_value=0)
+    return test_data
 
 
 if __name__ == '__main__':
